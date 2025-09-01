446f2691deba58f99b55b86430fade1c773e486f
Vasek - Tom C <tom.chauveau@epitech.eu>
2025-09-01 16:15:14 +0200

    add simple text generator with output dir
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- main.go
+++ main.go
@@ -8,9 +8,10 @@ import (
 type Textgen struct{}
 
 func (t *Textgen) GenerateClient(
-  ctx context.Context,
-  modSource *dagger.ModuleSource,
-  introspectionJSON *dagger.File,
+	ctx context.Context,
+	modSource *dagger.ModuleSource,
+	introspectionJSON *dagger.File,
+	outputDir string,
 ) (*dagger.Directory, error) {
-  return dag.Directory().WithNewFile("hello.txt", "hello world"), nil
+	return dag.Directory().WithDirectory(outputDir, dag.Directory().WithNewFile("hello.txt", "hello world")), nil
 }
